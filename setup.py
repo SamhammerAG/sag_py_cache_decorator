5b1f3efacc2202c5925c0826e2ed552622a86c6e
Saeer Noorani <snoorani@samhammer.de>
2025-01-24 15:05:23 +0100

    version update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements-dev.txt", "r") as fin:
 
 setuptools.setup(
     name="sag-py-cache-decorator",
-    version="0.3.0",
+    version="0.3.1",
     description="A decorator to cache method call results with similar parameters",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
